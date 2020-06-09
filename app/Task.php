9610b9719509645a2adeb2bfae4d69ba4b2513e6
Shun Ogawa <mm.shunogawa@gmail.com>
2020-06-09 23:59:28 +0630

    Add accessor for TaskStatus

--- app/Task.php
+++ app/Task.php
@@ -3,8 +3,47 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Task extends Model
 {
-    //
+    const STATUS = [
+    
+        1 => [ 'label' => 'Backlog', 'class' => 'label-danger'],
+        2 => [ 'label' => 'Doing', 'class' => 'label-info'],
+        3 => [ 'label' => 'Done', 'class' => '' ],
+    ];
+
+    public function getStatusLabelAttribute()
+
+    {
+        $status = $this->attributes['status'];
+         
+        if (!isset(self::STATUS[$status])) {
+            return '';
+        }
+        
+        return self::STATUS[$status]['label'];
+        
+    }        
+    
+    public function getStatusClassAttribute()
+    
+    {
+        $status = $this->attributes['status'];
+    
+        if (!isset(self::STATUS[$status])) {
+        return '';
+        }
+
+    return self::STATUS[$status]['class'];
+    }
+
+/*    public function getFormattedDueDateAttribute()
+
+    {
+          return Carbon::createFromFormat('Y-m-d', $this->attributes['due_date'])
+              ->format('Y/m/d');
+    }          
+*/
 }
